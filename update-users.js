87157e49b7a8b6115ddc11bddbf4e90cd143990f
Spokeek <spokeek@protonmail.com>
2023-08-07 17:04:32 +0200

    🚨 clean code

--- update-users.js
+++ update-users.js
@@ -5,21 +5,26 @@ const fs = require('fs')
 try {
     const event_json = JSON.parse(event_raw.split("\n").join(""))
     console.log(event_json)
-    const users = fs.readFileSync("./vrchat-kofi-users.txt", {encoding: "utf8"});
-    const userArray = users.toString().split("\n").filter(n => n.trim().length).map((line) => line.split(":").map((item) => item.trim()));
-    if(!userArray.find(line => line[0] === event_json.name)) {
+    const users = fs.readFileSync("./vrchat-kofi-users.txt", { encoding: "utf8" });
+    const userArray = users.toString()
+        .split("\n")
+        .filter(n => n.trim().length)
+        .map((line) => line.split(":")
+            .map((item) => item.trim()));
+
+    if (!userArray.find(line => line[0] === event_json.name)) {
         userArray.push([event_json.name, event_json.amount])
         console.log("user added")
     }
-    else{
+    else {
         console.log("nothing to add")
     }
     console.log(userArray)
 
     const textRaw = userArray.map((line) => line.join(" : ")).join("\n")
-    fs.writeFileSync("./vrchat-kofi-users.txt", textRaw, {encoding: "utf8"})
+    fs.writeFileSync("./vrchat-kofi-users.txt", textRaw, { encoding: "utf8" })
     console.log("file written")
 }
-catch(err) {
-    console.error({err, event_raw})
+catch (err) {
+    console.error({ err, event_raw })
 }
