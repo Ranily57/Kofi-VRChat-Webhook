f334e01a2a9f162d1d55b03d83d8c654a354763a
Spokeek <spokeek@protonmail.com>
2023-08-07 18:15:40 +0200

    ✅ Added test suite

--- update-users.js
+++ update-users.js
@@ -1,12 +1,14 @@
 // get content from first argument
-const event_raw = process.env.EVENT_RAW;
+const EVENT_RAW = process.env.EVENT_RAW;
+const FILE_PATH = process.env.FILE_PATH || "./vrchat-kofi-users.txt"
+
 const fs = require('fs')
 
 try {
-    const event_json = JSON.parse(event_raw.split("\n").join(""))
+    const event_json = JSON.parse(EVENT_RAW.split("\n").join(""))
     console.log(event_json)
 
-    const users = fs.readFileSync("./vrchat-kofi-users.txt", {encoding: "utf8"});
+    const users = fs.readFileSync(FILE_PATH, {encoding: "utf8"});
     const userArray = users.toString().split("\n").filter(n => n.trim().length).map((line) => line.split(":").map((item) => item.trim()));
     if(!userArray.find(line => line[0] === event_json.name)) {
         userArray.push([event_json.name, event_json.amount + "€"])
@@ -20,9 +22,9 @@ try {
     console.log(userArray)
 
     const textRaw = userArray.map((line) => line.join(" : ")).join("\n")
-    fs.writeFileSync("./vrchat-kofi-users.txt", textRaw, { encoding: "utf8" })
+    fs.writeFileSync(FILE_PATH, textRaw, { encoding: "utf8" })
     console.log("file written")
 }
 catch(err) {
-    console.error({err, event_raw})
+    console.error({err, EVENT_RAW})
 }
---
+++ update-users.test.js
@@ -0,0 +1,87 @@
+// Test Output file
+
+const fs = require('fs')
+const { execSync } = require("child_process");
+const process = require("process");
+const { on } = require('events');
+const SCRIPT_PATH = "./update-users.js"
+
+const TEST_DATA_FILE = "./test-data.txt"
+
+// User test 1 make a 3€ donation
+const EVENT_1 = {
+    "name": "user_test_1",
+    "amount": "3",
+    "type": "donation"
+}
+const EXPECT_EVENT_1 = [
+    ["user_test_1", "3€"]
+]
+
+// User test 1 updates and make a 5€ donation
+const EVENT_2 = {
+    "name": "user_test_1",
+    "amount": "5",
+    "type": "donation"
+}
+const EXPECT_EVENT_2 = [
+    ["user_test_1", "5€"]
+]
+
+// A new user makes a 10€ subscription
+const EVENT_3 = {
+    "name": "user_test_2",
+    "amount": "1",
+    "type": "subscription"
+}
+const EXPECT_EVENT_3 = [
+    ["user_test_1", "5€"],
+    ["user_test_2", "10€/mois"]
+]
+
+// Events to play
+const EVENTS = [
+    [EVENT_1, EXPECT_EVENT_1],
+    [EVENT_2, EXPECT_EVENT_2],
+    [EVENT_3, EXPECT_EVENT_3]
+]
+
+// remove data file
+try {
+    fs.unlinkSync(TEST_DATA_FILE)
+}
+catch (err) { }
+
+// create data file
+fs.writeFileSync(TEST_DATA_FILE, "", { encoding: "utf8" })
+
+let groupIndex = 0
+for (group of EVENTS) {
+    groupIndex++
+    const [event, expect] = group
+    const env = { "EVENT_RAW": JSON.stringify(event), "FILE_PATH": TEST_DATA_FILE };
+    process.stdout.write(execSync(`node ${SCRIPT_PATH}`, { env }).toString())
+    const users = fs.readFileSync(TEST_DATA_FILE, { encoding: "utf8" });
+    const expectRaw = expect.map((line) => line.join(" : ")).join("\n")
+    if (expectRaw !== users) {
+        console.error("event " + groupIndex + " failed")
+        console.error("expected:")
+        console.error(expectRaw)
+        console.error("got:")
+        console.error(users)
+        // remove data file
+        try {
+            fs.unlinkSync(TEST_DATA_FILE)
+        }
+        catch (err) { }
+
+        process.exit(1)
+    }
+    console.log("event " + groupIndex + " passed")
+}
+
+// remove data file
+try {
+    fs.unlinkSync(TEST_DATA_FILE)
+}
+catch (err) { }
