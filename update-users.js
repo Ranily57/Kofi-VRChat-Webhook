cd6c910f25cdc62941dd15d5cc5d098c43ed59e9
Spokeek <spokeek@protonmail.com>
2023-08-07 16:16:51 +0200

    💚 Using multi line JSON

--- update-users.js
+++ update-users.js
@@ -3,7 +3,7 @@ const event_raw = process.env.EVENT_RAW;
 const fs = require('fs')
 
 try {
-    const event_json = JSON.parse(event_raw)
+    const event_json = JSON.parse(event_raw.split("\n").join(""))
     console.log(event_json)
     const users = fs.readFileSync("./vrchat-kofi-users.txt", {encoding: "utf8"});
     const userArray = users.toString().split("\n").filter(n => n.trim().length).map((line) => line.split(":").map((item) => item.trim()));
